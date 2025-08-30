641e93c99ebd7118f5aee661677eb3e22776e3f3
warsana <inyomanwarsana1@gmail.com>
2025-08-30 09:41:26 +0800

    Understanding filter

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -30,6 +30,8 @@ public class AuthController {
 
     @PostMapping("/validate")
     public boolean validate(@RequestHeader("Authorization") String authHeader) {
+        System.out.println("Validate called");
+
         var token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
     }
