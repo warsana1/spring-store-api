cd7fc5d1e0da7988a9d5d080c16caba949654ea1
warsana <inyomanwarsana1@gmail.com>
2025-08-31 09:28:20 +0800

    Add role to users

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -28,6 +28,7 @@ public class JwtService {
         return Jwts.builder().subject(user.getId().toString())
                 .claim("email", user.getEmail())
                 .claim("name", user.getName())
+                .claim("role", user.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(jwtConfig.getSecretKey())
