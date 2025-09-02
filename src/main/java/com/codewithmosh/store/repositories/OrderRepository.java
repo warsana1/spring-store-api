0f10d6350a71ea2c89a22ab4d85db2c0b166c55c
warsana <inyomanwarsana1@gmail.com>
2025-09-02 08:43:54 +0800

    Get all orders

--- src/main/java/com/codewithmosh/store/repositories/OrderRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Order;
+import com.codewithmosh.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findOrderByCustomer(User customer);
 }
