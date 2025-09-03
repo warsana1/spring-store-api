74b43bf7bf58d2ec10f0676e194d753b6895b920
warsana <inyomanwarsana1@gmail.com>
2025-09-03 23:38:25 +0800

    Building a webhook endpoint

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -7,22 +7,46 @@ import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
 import com.codewithmosh.store.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkout")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutController {
     private final CheckoutService checkoutService;
+
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
 
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request)  {
            return checkoutService.checkout(request);
 
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(@RequestHeader("Stripe-Signature") String signature,@RequestBody String payload) {
+        try {
+            var event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+           var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+           switch (event.getType()) {
+               case "payment_intent.succeeded" -> {}
+               case "payment_intent.failed" -> {}
+           }
+           return ResponseEntity.ok().build();
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @ExceptionHandler(PaymentException.class)
